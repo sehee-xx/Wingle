ed82ebfb30be34c42b9f51101678fa916539089c
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2024-07-06 00:35:44 +0900

    Modify: 환경변수 설정

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ module.exports = {
   experimental: {
     appDir: true,
   },
+  env: {
+    NEXT_PUBLIC_KAKAO_REST_API_KEY: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
+    NEXT_PUBLIC_KAKAO_REDIRECT_URI: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_GOOGLE_REDIRECT_URI:
+      process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI,
+  },
   reactStrictMode: true,
 };
