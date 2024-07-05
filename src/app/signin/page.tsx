6defa5a58705616b07b2f9afedbac8478a5357f5
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2024-07-05 20:18:36 +0900

    Modify: background 이미지 등록

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -65,7 +65,7 @@ const LoginWrapper = styled.div`
   align-items: center;
   padding: 50px 50px;
   background-image: url("/assets/background.png");
-  /* background-color: #f7f8f9; */
+  /* background-color: #ffd453; */
 `;
 
 const LoginBox = styled.div`
