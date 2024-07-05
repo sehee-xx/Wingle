ca24be2977baf830c3462f68664512d290fe7456
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2024-07-06 02:20:45 +0900

    Modify: mypage 테블릿, 모바일 반응형

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -12,6 +12,13 @@ const Mypage = () => {
           <NameBox>
             <Name>양세희</Name>
             <NameOthers>님 안녕하세요!</NameOthers>
+            <DropdownWrapper>
+              <Dropdown>
+                <option value="latest">최신 순</option>
+                <option value="oldest">오래된 순</option>
+                <option value="most-liked">찜 많은 순</option>
+              </Dropdown>
+            </DropdownWrapper>
           </NameBox>
           <PostCard>
             <PostCardImg src="/assets/card1.png" />
@@ -161,9 +168,15 @@ const Mypage = () => {
         <Sidebar>
           <StickySidebar>
             <SidebarTitle>목록</SidebarTitle>
-            <SidebarItem>최신 순으로 보기</SidebarItem>
-            <SidebarItem>오래된 순으로 보기</SidebarItem>
-            <SidebarItem>찜하기 많은 순으로 보기</SidebarItem>
+            <SidebarItem className="latest" data-short="최신 순">
+              최신 순으로 보기
+            </SidebarItem>
+            <SidebarItem className="oldest" data-short="오래된 순">
+              오래된 순으로 보기
+            </SidebarItem>
+            <SidebarItem className="most-liked" data-short="찜 많은 순">
+              찜하기 많은 순으로 보기
+            </SidebarItem>
           </StickySidebar>
         </Sidebar>
       </MypageWrapper>
@@ -176,40 +189,68 @@ export default Mypage;
 const MypageWrapper = styled.div`
   display: flex;
   padding: 100px;
+
+  @media (max-width: 1024px) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  @media (max-width: 768px) {
+    padding-top: 90px;
+  }
 `;
 
 const Content = styled.div`
   flex: 3;
   display: flex;
   flex-direction: column;
+  padding-left: 150px;
+
+  @media (max-width: 1024px) {
+    padding-left: 10px;
+  }
 `;
 
 const Sidebar = styled.div`
   flex: 1;
   margin-left: 20px;
+
+  @media (max-width: 480px) {
+    display: none;
+  }
 `;
 
 const NameBox = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  width: 100%;
 `;
 
 const Name = styled.label`
   font-size: 24px;
   font-weight: 700;
   color: #ff812e;
+
+  @media (max-width: 480px) {
+    font-size: 20px;
+  }
 `;
 
 const NameOthers = styled.label`
   font-size: 20px;
   font-weight: 700;
   color: #303033;
+
+  @media (max-width: 480px) {
+    font-size: 16px;
+  }
 `;
 
 const PostCard = styled.div`
   width: 100%;
-  height: 300px;
+  max-width: 800px;
+  height: 100%;
   background-color: #f7f8f9;
   margin-top: 30px;
   display: flex;
@@ -219,12 +260,22 @@ const PostCard = styled.div`
   border-radius: 10px;
   gap: 15px;
   cursor: pointer;
+
+  @media (max-width: 480px) {
+    margin-top: 20px;
+    padding: 15px 15px;
+  }
 `;
 
 const PostCardImg = styled.img`
-  width: 50%;
-  height: 100%;
+  width: 200px;
+  height: 200px;
   border-radius: 10px;
+
+  @media (max-width: 480px) {
+    width: 100px;
+    height: 100px;
+  }
 `;
 
 const PostInfo = styled.div`
@@ -234,13 +285,13 @@ const PostInfo = styled.div`
 `;
 
 const PostName = styled.label`
-  font-size: 16px;
+  font-size: 18px;
   font-weight: 600;
   color: #303033;
 `;
 
 const PostSubData = styled.label`
-  font-size: 14px;
+  font-size: 16px;
   font-weight: 500;
   color: #303033;
 `;
@@ -251,27 +302,35 @@ const PostDescription = styled.div`
   color: #303033;
   overflow: hidden;
   display: -webkit-box;
-  -webkit-line-clamp: 8;
+  -webkit-line-clamp: 5;
   -webkit-box-orient: vertical;
   white-space: pre-wrap;
+
+  @media (max-width: 480px) {
+    display: none;
+  }
 `;
 
 const StickySidebar = styled.div`
   position: sticky;
   top: 100px;
+
+  @media (max-width: 480px) {
+    display: none;
+  }
 `;
 
 const SidebarTitle = styled.h2`
-  font-size: 14px;
+  font-size: 18px;
   font-weight: 700;
   color: #303033;
   margin-bottom: 10px;
 `;
 
 const SidebarItem = styled.div`
-  font-size: 12px;
+  font-size: 16px;
   margin-bottom: 10px;
-  padding: 10px;
+  padding: 15px;
   color: #303033;
   background-color: #f1f1f1;
   border-radius: 10px;
@@ -281,4 +340,47 @@ const SidebarItem = styled.div`
     background-color: #e66f1e;
     color: #ffffff;
   }
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+
+  @media (max-width: 480px) {
+    &.latest::after {
+      content: "최신 순";
+    }
+
+    &.oldest::after {
+      content: "오래된 순";
+    }
+
+    &.most-liked::after {
+      content: "찜 많은 순";
+    }
+
+    &.latest,
+    &.oldest,
+    &.most-liked {
+      font-size: 14px;
+      content: attr(data-short);
+    }
+  }
+`;
+
+const DropdownWrapper = styled.div`
+  display: none;
+
+  @media (max-width: 480px) {
+    display: block;
+    margin-left: auto;
+  }
+`;
+
+const Dropdown = styled.select`
+  padding: 5px;
+  font-size: 14px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  color: #303033;
+  font-size: 12px;
 `;
