a4dc0468e27e23b72e7fc68088f7436e42bf7dc4
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2024-07-05 15:46:01 +0900

    Modify: 메인 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,25 @@ import React from "react";
 import styled from "styled-components";
 import CardList from "../components/CardList";
 import Carousel from "../components/Carousel";
+import Header from "../components/Header";
 
 // src/app/page.tsx
-export default function Home() {
+const Home = () => {
   return (
     <HomeWrapper>
-      <Header>
-        <Logo src="/assets/logo.svg" alt="Logo" />
-        <SignBox>
-          <Signin>로그인</Signin>
-          <Signup>회원가입</Signup>
-          <Profile src="/assets/student.svg" />
-        </SignBox>
-      </Header>
+      <Header />
       <Body>
-        <Carousel></Carousel>
+        <Carousel />
       </Body>
       <Footer>
-        <CardList></CardList>
-        <FooterBanner src="/assets/bottom_banner.png" />
+        <CardList />
       </Footer>
+      <FooterBanner src="/assets/bottom_banner.png" />
     </HomeWrapper>
   );
-}
+};
+
+export default Home;
 
 const HomeWrapper = styled.div`
   padding: 5px 0px;
@@ -33,53 +29,12 @@ const HomeWrapper = styled.div`
   overflow-x: hidden;
 `;
 
-const Header = styled.div`
-  border-bottom: 1px solid #e1e1e2;
-  padding-bottom: 5px;
-  padding-left: 100px;
-  padding-right: 100px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+const Body = styled.div`
+  margin-top: 65px;
 `;
-
-const Logo = styled.img`
-  width: 100px;
-  cursor: pointer;
-`;
-
-const SignBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 10px;
-`;
-
-const Signin = styled.div`
-  font-size: 12px;
-  color: #303033;
-  cursor: pointer;
-`;
-
-const Signup = styled.div`
-  font-size: 12px;
-  color: #303033;
-  cursor: pointer;
-`;
-
-const Profile = styled.img`
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  cursor: pointer;
-`;
-
-const Body = styled.div``;
 
 const Footer = styled.div`
   padding: 50px 100px;
-  height: 300px;
 `;
 
 const FooterBanner = styled.img`
