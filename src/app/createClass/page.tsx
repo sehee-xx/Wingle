28e1f31828555cc91126ae2035a16bfebb988128
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2024-07-05 20:44:43 +0900

    Modify: 이미지 첨부 추가

--- src/app/createClass/page.tsx
+++ src/app/createClass/page.tsx
@@ -41,6 +41,10 @@ const CreateClass = () => {
             <SubLabel>클래스 설명</SubLabel>
             <InfoTextarea placeholder="클래스에 대해 설명해주세요" />
           </InfoGroup>
+          <InfoGroup>
+            <SubLabel>이미지 첨부</SubLabel>
+            <ImageUploadInput type="file" accept="image/*" />
+          </InfoGroup>
           <SubmitButton onClick={handleRegistClick}>등록하기</SubmitButton>
         </FormWrapper>
       </MakeClassWrapper>
@@ -112,6 +116,20 @@ const InfoTextarea = styled.textarea`
   padding: 10px 10px;
   height: 150px;
   resize: none;
+  box-sizing: border-box;
+  transition: border 0.3s;
+
+  &:focus {
+    border: 1px solid #ff812e;
+  }
+`;
+
+const ImageUploadInput = styled.input`
+  font-size: 14px;
+  color: #303033;
+  border-radius: 10px;
+  border: 1px solid #e1e1e3;
+  padding: 10px;
   box-sizing: border-box;
   transition: border 0.3s;
 
