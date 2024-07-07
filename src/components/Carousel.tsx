2ab99463c015514965e867c7dead398c43449b54
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2024-07-07 14:03:48 +0900

    Modify: 캐러셀 페이지 연결

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,12 +1,20 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-const Carousel = () => {
+const CarouselComponent = () => {
+  const [mounted, setMounted] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -17,115 +25,83 @@ const Carousel = () => {
     autoplaySpeed: 2000,
   };
 
+  const handleSlideClick = (id: number) => {
+    if (mounted) {
+      router.push(`/slide/${id}`);
+    }
+  };
+
   return (
     <>
-      <Slider {...settings}>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide1.png"
-              alt="Slide 1"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide2.png"
-              alt="Slide 2"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide3.png"
-              alt="Slide 3"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide4.png"
-              alt="Slide 4"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide5.png"
-              alt="Slide 5"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide6.png"
-              alt="Slide 6"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide7.png"
-              alt="Slide 7"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide8.png"
-              alt="Slide 8"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide9.png"
-              alt="Slide 9"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-        <div>
-          <ImageWrapper>
-            <Image
-              src="/assets/slide10.png"
-              alt="Slide 10"
-              layout="fill"
-              objectFit="cover"
-            />
-          </ImageWrapper>
-        </div>
-      </Slider>
+      {mounted && (
+        <Slider {...settings}>
+          <div onClick={() => handleSlideClick(1)}>
+            <ImageWrapper>
+              <Image
+                src="/assets/slide/slide1.png"
+                alt="Slide 1"
+                layout="fill"
+                objectFit="cover"
+              />
+            </ImageWrapper>
+          </div>
+          <div onClick={() => handleSlideClick(2)}>
+            <ImageWrapper>
+              <Image
+                src="/assets/slide/slide2.png"
+                alt="Slide 2"
+                layout="fill"
+                objectFit="cover"
+              />
+            </ImageWrapper>
+          </div>
+          <div onClick={() => handleSlideClick(3)}>
+            <ImageWrapper>
+              <Image
+                src="/assets/slide/slide3.png"
+                alt="Slide 3"
+                layout="fill"
+                objectFit="cover"
+              />
+            </ImageWrapper>
+          </div>
+          <div onClick={() => handleSlideClick(4)}>
+            <ImageWrapper>
+              <Image
+                src="/assets/slide/slide4.png"
+                alt="Slide 4"
+                layout="fill"
+                objectFit="cover"
+              />
+            </ImageWrapper>
+          </div>
+          <div onClick={() => handleSlideClick(5)}>
+            <ImageWrapper>
+              <Image
+                src="/assets/slide/slide5.png"
+                alt="Slide 5"
+                layout="fill"
+                objectFit="cover"
+              />
+            </ImageWrapper>
+          </div>
+          <div onClick={() => handleSlideClick(6)}>
+            <ImageWrapper>
+              <Image
+                src="/assets/slide/slide6.png"
+                alt="Slide 6"
+                layout="fill"
+                objectFit="cover"
+              />
+            </ImageWrapper>
+          </div>
+        </Slider>
+      )}
     </>
   );
 };
 
-export default Carousel;
+export default CarouselComponent;
 
 const ImageWrapper = styled.div`
   position: relative;
