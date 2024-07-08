2f39b48c53c1809502e84e0e6f11d41ab2a8cd2b
sehee-xx <104626753+sehee-xx@users.noreply.github.com>
2024-07-09 05:07:05 +0900

    Modify: 카드 전체 보기 기능 구현

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -1,15 +1,39 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
 import { useRouter } from "next/navigation";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const CardList = () => {
   const router = useRouter();
   const [searchQuery, setSearchQuery] = useState("");
   const [userType, setUserType] = useState<string>("");
+  const [cards, setCards] = useState<any[]>([]);
+  const [sortType, setSortType] = useState<string>("최신순");
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.BACKEND_HOSTNAME}/courses`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        const data = response.data;
+        setCards(data.trending);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
     const storedUserType = localStorage.getItem("userType");
     if (storedUserType) {
       setUserType(storedUserType);
@@ -28,176 +52,102 @@ const CardList = () => {
     setSearchQuery(e.target.value);
   };
 
-  const cards = [
-    {
-      id: 1,
-      title: "강아지 간식 만들기",
-      description: "건강한 간식을 만들어보세요!",
-      image: "/assets/card/card1.png",
-      price: "₩45,000",
-    },
-    {
-      id: 2,
-      title: "해금 원데이 클래스",
-      description: "하루만에 해금 마스터 하기",
-      image: "/assets/card/card2.png",
-      price: "₩50,000",
-    },
-    {
-      id: 3,
-      title: "나만의 명함 만들기",
-      description: "특색있는 명함을 만들 수 있습니다",
-      image: "/assets/card/card3.png",
-      price: "₩35,000",
-    },
-    {
-      id: 4,
-      title: "일러스트 클래스",
-      description: "일러스트 기초부터 탄탄하게!",
-      image: "/assets/card/card4.png",
-      price: "₩70,000",
-    },
-    {
-      id: 5,
-      title: "나만의 향수 만들기",
-      description: "향수로 자신을 표현해보세요",
-      image: "/assets/card/card5.png",
-      price: "₩60,000",
-    },
-    {
-      id: 6,
-      title: "앙금 케이크 클래스",
-      description: "앙금으로 케이크를 만들어보아요",
-      image: "/assets/card/card6.png",
-      price: "₩80,000",
-    },
-    {
-      id: 7,
-      title: "도자기 만들기",
-      description: "초보자도 쉽게 할 수 있는 클래스!",
-      image: "/assets/card/card7.png",
-      price: "₩60,000",
-    },
-    {
-      id: 8,
-      title: "꽃꽂이 클래스",
-      description: "꽃과 함께 하는 시간",
-      image: "/assets/card/card8.png",
-      price: "₩55,000",
-    },
-    {
-      id: 9,
-      title: "오늘은 내가 밴드 보컬?",
-      description: "일일 밴드 보컬 체험해보세요!",
-      image: "/assets/card/card9.png",
-      price: "₩50,000",
-    },
-    {
-      id: 10,
-      title: "수제 맥주 클래스",
-      description: "수제 맥주 만들기",
-      image: "/assets/card/card10.png",
-      price: "₩65,000",
-    },
-    {
-      id: 11,
-      title: "퍼스널 컬러 진단",
-      description: "베스트 컬러를 진단해드려요",
-      image: "/assets/card/card11.png",
-      price: "₩90,000",
-    },
-    {
-      id: 12,
-      title: "앙렉스X서핑캠프",
-      description: "다같이 서핑해요-!",
-      image: "/assets/card/card12.png",
-      price: "₩125,000",
-    },
-    {
-      id: 13,
-      title: "힐링 미술 클래스",
-      description: "취미 미술 초보반",
-      image: "/assets/card/card13.png",
-      price: "₩80,000",
-    },
-    {
-      id: 14,
-      title: "꽃꽂이 원데이 클래스",
-      description: "아름다운 화분 만들기",
-      image: "/assets/card/card14.png",
-      price: "₩65,000",
-    },
-    {
-      id: 15,
-      title: "물레 체험 클래스",
-      description: "초보자도 쉽게 할 수 있어요",
-      image: "/assets/card/card15.png",
-      price: "₩70,000",
-    },
-    {
-      id: 16,
-      title: "메이크업 클래스",
-      description: "내 찰떡 메이크업은?",
-      image: "/assets/card/card16.png",
-      price: "₩50,000",
-    },
-    {
-      id: 17,
-      title: "팬케이크 만들기",
-      description: "요리 초보도 간단하게!",
-      image: "/assets/card/card17.png",
-      price: "₩45,000",
-    },
-    {
-      id: 18,
-      title: "테니스 원데이 클래스",
-      description: "함께 테니스 쳐요",
-      image: "/assets/card/card18.png",
-      price: "₩50,000",
-    },
-    {
-      id: 19,
-      title: "화분 꾸미기",
-      description: "나만의 화분 만들기",
-      image: "/assets/card/card19.png",
-      price: "₩50,000",
-    },
-    {
-      id: 20,
-      title: "원데이 일러스트 클래스",
-      description: "색감 위주의 수업",
-      image: "/assets/card/card20.png",
-      price: "₩65,000",
-    },
-    {
-      id: 21,
-      title: "드로잉 클래스",
-      description: "원하는 것을 모두 그려보세요!",
-      image: "/assets/card/card21.png",
-      price: "₩50,000",
-    },
-    {
-      id: 22,
-      title: "믹싱 원데이 클래스",
-      description: "좋아하는 음악 직접 만들기",
-      image: "/assets/card/card22.png",
-      price: "₩50,000",
-    },
-    {
-      id: 23,
-      title: "키링 만들기 클래스",
-      description: "귀여운 키링을 하루만에?",
-      image: "/assets/card/card23.png",
-      price: "₩30,000",
-    },
-    {
-      id: 24,
-      title: "인형 만들기 클래스",
-      description: "쉽고 간단하게 만드는 인형",
-      image: "/assets/card/card24.png",
-      price: "₩35,000",
-    },
-  ];
+  const handleSortChange = (sortOption: string) => {
+    setSortType(sortOption);
+    let sortedCards = [...cards];
+    switch (sortOption) {
+      case "최신순":
+        sortedCards = sortedCards.sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        );
+        break;
+      case "인기순":
+        sortedCards = sortedCards.sort((a, b) => b.numWishes - a.numWishes);
+        break;
+      case "가격순":
+        sortedCards = sortedCards.sort(
+          (a, b) =>
+            parseFloat(a.price.replace("₩", "").replace(",", "")) -
+            parseFloat(b.price.replace("₩", "").replace(",", ""))
+        );
+        break;
+      default:
+        break;
+    }
+    setCards(sortedCards);
+  };
+
+  const toggleLike = async (card: any) => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      Swal.fire({
+        icon: "warning",
+        title: "Unauthorized",
+        text: "Please log in to add to your wish list.",
+      });
+      return;
+    }
+
+    const isLiked = card.isFavorite;
+
+    // UI를 먼저 업데이트하여 즉시 반영
+    updateCardFavoriteStatus(card.id, !isLiked);
+
+    try {
+      if (isLiked) {
+        const response = await axios.delete(
+          `${process.env.BACKEND_HOSTNAME}/courses/${card.id}/wish`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.status !== 200) {
+          throw new Error("Failed to delete from wishlist");
+        }
+      } else {
+        const response = await axios.post(
+          `${process.env.BACKEND_HOSTNAME}/courses/${card.id}/wish`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.status > 201) {
+          throw new Error("Failed to add to wishlist");
+        }
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 409) {
+        Swal.fire({
+          icon: "info",
+          title: "Already in wish list",
+          text: "This item is already in your wish list.",
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Failed to toggle like",
+          text: error.response ? error.response.data.message : error.message,
+        });
+      }
+    }
+  };
+
+  const updateCardFavoriteStatus = (id: number, isFavorite: boolean) => {
+    setCards((prevState) => {
+      const updateCards = (cards: any[]) =>
+        cards.map((card) => (card.id === id ? { ...card, isFavorite } : card));
+
+      return updateCards(prevState);
+    });
+  };
 
   const filteredCards = cards.filter((card) =>
     card.title.includes(searchQuery)
@@ -214,9 +164,15 @@ const CardList = () => {
           onChange={handleSearchChange}
         />
         <SortFilterWrapper>
-          <SortFilterButton>최신순</SortFilterButton>
-          <SortFilterButton>인기순</SortFilterButton>
-          <SortFilterButton>가격순</SortFilterButton>
+          <SortFilterButton onClick={() => handleSortChange("최신순")}>
+            최신순
+          </SortFilterButton>
+          <SortFilterButton onClick={() => handleSortChange("인기순")}>
+            인기순
+          </SortFilterButton>
+          <SortFilterButton onClick={() => handleSortChange("가격순")}>
+            가격순
+          </SortFilterButton>
           {userType === "expert" && (
             <CreateClass onClick={handleCreateClick}>
               클래스 등록하기
@@ -231,6 +187,20 @@ const CardList = () => {
                 <CardTitle>{card.title}</CardTitle>
                 <CardDescription>{card.description}</CardDescription>
                 <CardPrice>{card.price}</CardPrice>
+                {userType === "student" && (
+                  <LikeIcon
+                    src={
+                      card.isFavorite
+                        ? "/assets/fillHeart.png"
+                        : "/assets/emptyHeart.png"
+                    }
+                    alt="Like Icon"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      toggleLike(card);
+                    }}
+                  />
+                )}
               </CardContent>
             </Card>
           ))}
@@ -368,6 +338,7 @@ const CardImg = styled.img`
 
 const CardContent = styled.div`
   padding: 15px;
+  position: relative;
 `;
 
 const CardTitle = styled.label`
@@ -403,6 +374,15 @@ const CardPrice = styled.div`
   @media (max-width: 768px) {
     font-size: 14px;
   }
+`;
+
+const LikeIcon = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
 `;
 
 const CreateClass = styled.div`
